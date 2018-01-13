1b16208ce8ebad5e10d3c7bc2ac8f94956644279
Gabe <Gabe@Mikes-MacBook-Pro.local>
2018-01-13 15:54:21 -0500

    Added Yahoo Weather API

--- javascript/custom.js
+++ javascript/custom.js
@@ -56,9 +56,9 @@ $("#submitBtn").on("click", function(event) {
 
   //AJAX Request for foursquare
   // Put foursquare query here
-  var queryURL = "https://api.foursquare.com/v2/venues/explore?" + "mode=url" + "&near=" + userCityandState + "&limit=6" + "&client_id=" + client_id + "&client_secret=" + client_secret + "&v=20180112" + "&q=Fun";
+  var queryURL2 = "https://api.foursquare.com/v2/venues/explore?" + "mode=url" + "&near=" + userCityandState + "&limit=6" + "&client_id=" + client_id + "&client_secret=" + client_secret + "&v=20180112" + "&q=Fun";
   $.ajax({
-    url: queryURL,
+    url: queryURL2,
     method: "GET"
   }).done(function(response) {
     console.log(response);
@@ -66,6 +66,68 @@ $("#submitBtn").on("click", function(event) {
 
 
   // Put Ajax Weather query here
+  let lat = "";
+
+        let long  = "";
+
+        let cityName = userCityandState
+
+        let queryURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"
+
+        + cityName + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+         $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+        .done(function(response) {
+          console.log(queryURL);
+
+          console.log(response);
+
+          $('#city').text(response.query.results.channel.location.city + ", " + response.query.results.channel.location.region);
+
+          let icon = "<img src='http://l.yimg.com/a/i/us/we/52/" + response.query.results.channel.item.condition.code + ".gif'>"
+
+          $('#icon').html(icon);
+          $('#temp').html(response.query.results.channel.item.condition.temp + "&#8457");
+          $('#condition').text(response.query.results.channel.item.condition.text);
+
+          lat = response.query.results.channel.item.lat;
+
+          long = response.query.results.channel.item.long;
+
+          console.log(lat);
+          console.log(long);
+
+          for(i = 1; i < 5; i++) {
+
+            let icon = $("<img>");
+
+            icon.attr("src", "http://l.yimg.com/a/i/us/we/52/" + response.query.results.channel.item.forecast[i].code + ".gif");
+            icon.attr("class", "icon");
+
+            let div = $("<div>");
+
+            div.attr("id", "forecast-day" + i);
+            div.attr("class", "forecast");
+
+
+            $('#forecast').append(div);
+
+            $('#forecast-day' + i).html(response.query.results.channel.item.forecast[i].day + "  " + response.query.results.channel.item.forecast[i].high + "&#8457");
+
+            $('#forecast-day' + i).append(icon);
+
+            }
+
+
+});
+
+
+
+
 
 
 
